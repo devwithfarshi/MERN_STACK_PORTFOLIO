8a837eaf3a0773ee064238b07e4378302583a189
Salman Farshi <salman@kali>
2023-04-14 16:28:05 +0600

    Ready For deploy

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use("/api/v1", userRouter);
 
