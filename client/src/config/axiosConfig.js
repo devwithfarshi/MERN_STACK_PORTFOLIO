d4568424afcb20a8519e773b6ddde594aba9f60b
Salman Farshi <salman@kali>
2023-04-14 23:29:43 +0600

    Ready For deploy

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const myAx = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
   // baseURL: "http://192.168.0.104:5000",
+  baseURL: "https://salmanportfolioserver.onrender.com/",
 });
 
 export default myAx;
