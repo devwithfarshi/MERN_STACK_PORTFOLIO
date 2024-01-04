9b3ac6195d50bca33d8bf84103a465f1192440a6
Salman Farshi <kingsalman0102@gmail.com>
2024-01-05 00:16:21 +0600

    fix too large bug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const path = require("path");
 const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
@@ -11,7 +12,13 @@ const app = express();
 dotenv.config();
 connectDatabase();
 app.use(cors());
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
+app.use(express.urlencoded({ limit: "50mb" }));
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,10 +26,17 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//     parameterLimit: 100000,
+//     limit: "500mb",
+//   })
+// );
+// // parse application/json
+// app.use(bodyParser.json());
+
 app.use("/api/v1", userRouter);
 
 app.get("/", (req, res) => {
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 import "./Footer.css";
@@ -11,46 +11,31 @@ const Footer = ({ about }) => {
     setIsShowMore(!isShowMore);
   };
   return (
-    <div className='footer'>
+    <div className="footer">
       <div>
-        <Typography variant='h5'>About Me</Typography>
+        <Typography variant="h5">About Me</Typography>
         <Typography fontSize={isShowMore && "14px"}>
           {isShowMore ? `Greetings, ${text}..` : `${text.slice(0, 95)} ...`}
-          <span
-            onClick={showMore}
-            className='showBtn'
-          >
+          <span onClick={showMore} className="showBtn">
             {!isShowMore ? "show more" : "hide"}
           </span>
         </Typography>
 
-        <Link
-          to='/contact'
-          className='footerContactBtn'
-        >
+        <Link to="/contact" className="footerContactBtn">
           <Typography>Contact Us</Typography>
         </Link>
       </div>
       <div>
-        <Typography variant='h6'>Social Media</Typography>
-        <div className='socialLinks'>
-          <a
-            href='https://github.com/devwithfarshi/'
-            target='black'
-          >
+        <Typography variant="h6">Social Media</Typography>
+        <div className="socialLinks">
+          <a href="https://github.com/devwithfarshi/" target="black">
             <BsGithub />
           </a>
 
-          <a
-            href='https://www.instagram.com/__sf.__.salman___/'
-            target='black'
-          >
+          <a href="https://www.instagram.com/__sf.__.salman___/" target="black">
             <BsInstagram />
           </a>
-          <a
-            href='https://www.linkedin.com/in/salman-farshi-a7b8b5254/'
-            target='black'
-          >
+          <a href="https://www.linkedin.com/in/devwithfarshi/" target="black">
             <BsLinkedin />
           </a>
         </div>
